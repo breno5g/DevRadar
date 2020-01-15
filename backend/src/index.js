f8ec9282a9b514b1979ab11ede1a29244c24cd57
Breno Santos <brenosantos145@gmail.com>
2020-01-15 12:05:59 -0300

    Initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,17 @@
+const express = require('express') 
+const mongoose = require("mongoose")
+const routes = require("./routes")
+
+const app = express()
+
+mongoose.connect("mongodb+srv://breno:rotacomando1@cluster0-3etda.mongodb.net/week10?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+})
+
+app.use(express.json())
+app.use(routes)
+
+app.listen(3333)
