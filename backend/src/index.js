e4d2750dec0c730614c2d75c9c32f7cd6e620cb7
Breno Santos <brenosantos145@gmail.com>
2020-01-16 10:59:49 -0300

    update

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,18 @@
 const express = require('express') 
 const mongoose = require("mongoose")
+const cors = require ("cors")
 const routes = require("./routes")
 
 const app = express()
 
-mongoose.connect("mongodb+srv://breno:rotacomando1@cluster0-3etda.mongodb.net/week10?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://breno:omnistack@cluster0-3etda.mongodb.net/week10?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
     useCreateIndex: true,
 })
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
